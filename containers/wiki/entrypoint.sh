844e0aaf58a4ea34591316d9b90e2a05c5425fac
Denys Bashkatov <denys.bashkatov@seetree.co>
2025-06-05 01:18:43 +0300

    fixed wiki entrypoint

--- containers/wiki/entrypoint.sh
+++ containers/wiki/entrypoint.sh
@@ -9,17 +9,15 @@ ZIM_FILES=$(find /kiwix-data -name "*.zim" -type f | tr '\n' ' ')
 
 if [ -z "$ZIM_FILES" ]; then
     echo "No .zim files found in /kiwix-data"
-    exit 1
 fi
 
 for file in $ZIM_FILES; do
     if [ ! -r "$file" ]; then
         echo "Error: Cannot read ZIM file: $file"
-        exit 1
     fi
     echo "Found readable ZIM file: $file"
 done
 
 echo "Starting kiwix-serve..."
 
-exec ./kiwix-serve --verbose --port 8080 --library /kiwix/tools/library.xml $ZIM_FILES
+exec ./kiwix-serve --verbose --port 8080 $ZIM_FILES
