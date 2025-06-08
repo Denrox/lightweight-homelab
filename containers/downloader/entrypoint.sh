a8a711b3bc057de078a98236d12b768caa721324
Denys Bashkatov <denys.bashkatov@seetree.co>
2025-06-08 20:50:41 +0300

    make downloader more often

--- containers/downloader/entrypoint.sh
+++ containers/downloader/entrypoint.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+# Start the Docker daemon with TLS enabled
+dockerd-entrypoint.sh dockerd &
+
+# Wait for Docker daemon to be ready
+until docker info > /dev/null 2>&1; do
+    echo "Waiting for Docker daemon..."
+    sleep 1
+done
+
 cd /usr/local/bin/scripts && ./cronscript.sh
 
-cron
+crond -f -d 8 &
 
 tail -f /var/log/cron.log
